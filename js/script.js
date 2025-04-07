65b9c354d133f9551228a9354a4d7a44bc7e76b1
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-07 18:15:48 +0200

    [UPD] ciclo l'array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 
 //RECUPERO ELEMENTI NEL DOM
 
+const pos = document.getElementById(`num`)
+const foto = document.getElementById(`foto`)
+const data = document.getElementById(`date`)
+const luogo = document.getElementById(`zona`)
+const riga = document.querySelector(`.row`) 
 
-// CHIAMATA AXIOS
+//DICHIARO VARIABILE VUOTA DOVE INSERIRE SUCCESIVAMENTE HTML
+let colGen = '';
+
+// CHIAMATA AXIOS E CICLO ARRAY
 
 const contenutoCard = `https://lanciweb.github.io/demo/api/pictures/`
 
-const ciclare = axios.get(contenutoCard).then((card)=>{
-    console.log(card.data)
+axios.get(contenutoCard).then((card)=>{
+    const array = card.data
+
+    console.log(array)
+
+    array.forEach((elementi) => {
+        posNum = elementi
+        console.log(posNum)
+        
+
+        
+    });
 }) 
+
