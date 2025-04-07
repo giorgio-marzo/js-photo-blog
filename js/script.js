b7582c58878c3e782a96599140e19ad38ff1ca12
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-07 17:04:42 +0200

    [UPD] recupero array con id da usare

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 
 //RECUPERO ELEMENTI NEL DOM
+
+
+// CHIAMATA AXIOS
+
+const contenutoCard = `https://lanciweb.github.io/demo/api/pictures/`
+
+const ciclare = axios.get(contenutoCard).then((card)=>{
+    console.log(card.data)
+}) 
