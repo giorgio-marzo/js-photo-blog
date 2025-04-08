039e6dcfdc929d6bbb522234934640b05c0742b4
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-08 21:22:29 +0200

    [ERROR] funzione non prende la nodelist polaroid

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 
 //RECUPERO ELEMENTI NEL DOM
 
-/*const pos = document.getElementById(`num`)
-const foto = document.getElementById(`foto`)
+const pos = document.getElementById(`num`)
+/*const foto = document.getElementById(`foto`)
 const data = document.getElementById(`date`)
 const luogo = document.getElementById(`zona`)*/
-const riga = document.querySelector(`.row`) 
-const hoverlay = document.getElementById(`hoverlay`)
-const chiudi = document.getElementById(`bottone`)
-const card = document.querySelectorAll(`polaroid`)
+
+// QUESTI ID NON VANNO BENE PERCHE SONO AGGIUNTI 6 VOLTE. ID = "1 VOLTA SOLA"
+
+const riga = document.querySelector(`.row`)
 
 
-//DICHIARO VARIABILE VUOTA DOVE INSERIRE SUCCESIVAMENTE HTML
+
+
+//DICHIARO VARIABILI VUOTA DOVE INSERIRE SUCCESIVAMENTE HTML
 let colGen = '';
+
 
 // CHIAMATA AXIOS E CICLO ARRAY
 
 const contenutoCard = `https://lanciweb.github.io/demo/api/pictures/`
 
-axios.get(contenutoCard).then((card)=>{
+axios.get(contenutoCard).then((card)=>{ //INZIO FUNZIONE CHE CONTIENE TUTTO PROGRAMMA ASINCRONO
     const array = card.data
 
     console.log(array)
 
-    array.forEach((elementi) => {
+    // ciclco array
+    array.forEach((elementi) => { // INIZIO CICLO
         posNum = elementi
         
         //destrutturo array
@@ -32,39 +36,67 @@ axios.get(contenutoCard).then((card)=>{
 
         //creo le col da appendere
 
-        colGen += `<div class="colu-lg-3" id="${id}">
-                    <div class="polaroid">
+        colGen += `<div class="colu-lg-3">
+                    <div class="polaroid posizione${id}">
                         <div class="margine">
 
                             <div class="puntina">
                                 <img src="./img/pin.svg" alt="">
                             </div>
 
-                                <div class="fotografia d-flex js-center" id="foto">
+                                <div class="fotografia d-flex js-center" >
                                     <img class="sfondo" src=${url} alt="">
                                 </div>
-                            <div class="data" id="date">${date}</div>
-                            <div class="luogo titoli " id="zona"><h2>${title.toUpperCase()}</h2></div>
+                            <div class="data" >${date}</div>
+                            <div class="luogo titoli " ><h2>${title.toUpperCase()}</h2></div>
                             
                         </div>
                     </div>
                 </div>`
+
+
+       
+
         
-    });
+    }); // FINE CICLO
 
 
     riga.innerHTML = colGen
+    
+
+
+    
+
+    // definisco variabile (dentro axios) che prende nel tutti gli elementi con "polaroid"
+    const polaroid = document.querySelectorAll(`.pol aroid`)
+
+    console.log(polaroid)
+   
+
+    const hoverlay = document.querySelector(`.hoverlay`)
+    const chiudi = document.querySelector(`.bottone`)
+    
 
 
 
+    // funzione che rimuove dalla classe hovelay "d-none" rimuovendo hoverlay
     chiudi.addEventListener(`click`, function(){
 
         hoverlay.classList.add(`d-none`)   
-    
-    
+        
+    })
+
+    //funziune che aggiunge di nuovo hoverlay
+
+    polaroid.addEventListener(`click`, function(){
+
+        hoverlay.classList.add(`d-none`)   
+        
     })
 
 
+}) // FINE ASINCRONO
 
-}) 
+
+
 
